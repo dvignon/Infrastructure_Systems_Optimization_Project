886ee177d7fad6acbf2dae11253c4b2829dfb220
dvignon <danielvignon70@gmail.com>
2017-11-17 17:46:21 -0500

    New update
    Updated function getDemand.py

--- Code/getDemand.py
+++ Code/getDemand.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+from scipy import sparse
 def getDemand(filename): 
     """This function returns the demand for each O-D pair in the TAP problem
     
@@ -7,4 +10,24 @@ def getDemand(filename):
     OUTPUTS:
     d: Numpy array where d[i,j] is the demand between node i and node j. d[i,i] = 0
     """
-    return d
+    
+    #Here we open the file
+    f  = open(filename, "r")
+    
+    #Parse through the file line by line
+    for line in f.readlines():
+    #Get the number of O_D pairs and creates a matrix for these O-D pairs
+        if "<NUMBER OF ZONES>" in line: 
+            n_origin = int(line.split(">")[1])
+            D = np.zeros([n_origin,n_origin])
+        #Check number of Origin
+        elif "Origin" in line: 
+            index_i=int(line.split("\t")[1])
+        #Fills row for Origin index_i
+        else:
+            index_j =line.replace(";",":").split(":") 
+            for i in range(0,len(index_j)-2,2):
+                D[index_i-1,int(index_j[i].replace(" ",""))-1]=float(index_j[i+1].replace(" ",""))
+    
+    
+    return D
--- Code/getParam.py
+++ Code/getParam.py
@@ -1,8 +1,11 @@
+import pandas as pd
+import numpy as np
+from scipy import sparse
 def getParam(filename):
     """This function returns the parameters for the TAP problem
     
     INPUTS:
-    filename: Name of the file from which the parameters for the problem are read.
+    filename: (relative) Path of the file from which the parameters for the problem are read.
                This is a string
     
     OUTPUTS:
@@ -14,8 +17,28 @@ def getParam(filename):
     A: list of tuples A[k]=(i,j) where i (j) is the head (tail) of link k
     M: Sparse node incidence matrix M
     """
+    #Reading the parameters from the file
+    param = pd.read_csv(filename, sep = '\t', comment = '<',usecols=['Capacity ', 'Free Flow Time ', 'B', 'Power'])
     
+    #Renaming the columns of the file according to our conventions
+    param.columns = ['C','t_0','beta','alpha']
     
+    #Reading link informations for the file and converting to an array of tuples
+    A = pd.read_csv(filename, sep = '\t', comment = '<',usecols=['Init node ', 'Term node '])
+    A = list(zip(A['Init node '], A['Term node ']))
+    
+    #Getting number of nodes(N) and links(L)
+    N = max(max(A,key=lambda x:x[0])[0],max(A,key=lambda x:x[1])[1])
+    L = len(param)
+    
+    #Build sparse incidence matrix
+    M= np.zeros((N,L))
+    for a,b in zip(A,range(len(A))):
+        M[a[0]-1,b] = 1;
+        M[a[1]-1,b] = -1;
+
+    M = sparse.csr_matrix(M)
+
     return param, A, M
     
     
