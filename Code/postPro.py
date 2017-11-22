4fbd25b24da78df948ff8f23881cbdae4eaa6948
dvignon <danielvignon70@gmail.com>
2017-11-22 17:40:40 -0500

    Update
    New constraint definition method

--- Code/postPro.py
+++ Code/postPro.py
@@ -1,5 +1,6 @@
-def postPro(results,filename,_format)
-"""
+import csv
+def postPro(results):
+    """
     This function creates saves the results into a specified format and filename
     
     INPUTS: 
@@ -10,6 +11,35 @@ def postPro(results,filename,_format)
     OUTPUTS:
     None
 
-"""
-
-
+    """
+    #
+    # Collect the data
+    #
+    vars = set()
+    data = {}
+    f = {}
+    for i in range(len(results.solution)):
+        data[i] = {}
+        for var in results.solution[i].variable:
+            vars.add(var)
+            data[i][var] = \
+            results.solution[i].variable[var]['Value']
+            
+        f[i] = results.solution[i].objective['obj']['Value']
+    #
+    # Write a CSV file, with one row per solution.
+    # The first column is the function value, the remaining
+    # columns are the values of nonzero variables.
+    #
+    rows = []
+    vars = list(vars)
+    vars.sort()
+    rows.append(['obj']+vars)
+    for i in range(len(results.solution)):
+        row = [f[i]]
+        for var in vars:
+            row.append( data[i].get(var,None) )
+            rows.append(row)
+            
+    print("Creating results file results.csv")
+    writer = csv.writer(open('results.csv', 'wb'))
