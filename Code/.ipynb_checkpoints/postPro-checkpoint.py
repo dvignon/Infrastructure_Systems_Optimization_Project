dce7ca5e3a86d56f6548060d3478534addeb36da
dvignon <danielvignon70@gmail.com>
2017-12-03 20:46:04 -0500

    Update

--- Code/.ipynb_checkpoints/postPro-checkpoint.py
+++ Code/.ipynb_checkpoints/postPro-checkpoint.py
@@ -22,8 +22,7 @@ def postPro(results):
         data[i] = {}
         for var in results.solution[i].variable:
             vars.add(var)
-            data[i][var] = \
-            results.solution[i].variable[var]['Value']
+            data[i][var] = results.solution[i].variable[var]['Value']
             
         f[i] = results.solution[i].objective['obj']['Value']
     #
--- Code/objRule.py
+++ Code/objRule.py
@@ -1,4 +1,4 @@
-def objRule(model):
+def objRule(model,param):
     """
     This function defines the obejctive function for the TAP
     
@@ -11,5 +11,5 @@ def objRule(model):
     Objective function expression (Concrete or Abstract)
     """
 
-    #return sum(param['t_0'][a]*model.f[a]*(1+ param['beta'][a]/(param['alpha'][a]+1)*(model.f[a]/param['C'][a])**param['alpha'][a]) for a in model.links)
-    return sum(model.c[a] for a in model.links)
+    return sum(param['t_0'][a]*model.f[a]*(1+ param['beta'][a]/(param['alpha'][a]+1)*(model.f[a]/param['C'][a])**param['alpha'][a]) for a in model.links)
+    #return sum(model.c[a] for a in model.links)
